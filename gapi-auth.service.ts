bdf09bde68bf10c1b70ef632a47378987b145dff
IhorKulinich <78841262+IhorKulinich@users.noreply.github.com>
2024-01-31 04:05:48 +0200

    cosmetic

--- gapi-auth.service.ts
+++ gapi-auth.service.ts
@@ -8,35 +8,54 @@ import { environment } from '../../../../environments/environment';
     providedIn: 'root'
 })
 export class AuthService {
+
     gAuth: any;
-    constructor() {
-    }
+
+    constructor() {}
 
     initOAuth(): Promise<any>{
-        return gapi.load('auth2', () => {
+
+        return gapi.load( 'auth2', () => {
+
             gapi.auth2.init({
+
                 client_id: environment.gapiId,
+
                 scope: environment.gapiEmail // note "client_id", not "apiKey"
-            }).then((auth2: any) => {
-                if (!auth2.isSignedIn.get()) { // check if already signed in
+
+            }).then( ( auth2: any ) => {
+
+                if ( !auth2.isSignedIn.get() ) { // check if already signed in
+
                     this.signInAuth(auth2);
+
                 }
+
             });
+
         });
+
     }
 
-    signInAuth(auth2: any){
-        auth2.signIn().then((response: any) => {
+    signInAuth( auth2: any ){
+
+        auth2.signIn().then( ( response: any ) => {
+
             console.log("sign in progress")
             
             const authResponse = response.currentUser.get().getAuthResponse();
-            sessionStorage.setItem('accessToken', authResponse.access_token);
+
+            sessionStorage.setItem( 'accessToken', authResponse.access_token );
             
             console.log("access token:");
-            console.log(authResponse.access_token);
+
+            console.log( authResponse.access_token );
+
         })
-        .catch(function(err: any) {
+        .catch( ( err: any ) => {
+
           console.log(err);
+
         })
     }
 }
--- homepage.component.ts
+++ homepage.component.ts
@@ -7,51 +7,79 @@ import { DocService } from '../shared/services/medium/gapi-doc.service';
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
+
 export class HomepageComponent implements OnInit {
+  
   apiLoaded!: boolean;
+
   apiFailed!: boolean;
+
   apiReady!: boolean;
+
   authReady!: boolean;
+
   authFailed!: boolean;
 
-  constructor(
-    private serviceDoc: DocService, 
-    private serviceAuth: AuthService,
-  ){
-    
-  }
+  constructor( private serviceDoc: DocService, private serviceAuth: AuthService ){}
 
   ngOnInit(): void {
 
     this.serviceAuth.initOAuth().then(
-      (result: any) => {
+
+      ( result: any ) => {
+
         console.log("access token");
+
         console.log(sessionStorage.getItem('accessToken'));
 
         this.authReady = true;
+
         return this.serviceDoc.loadClient().then(
-          (result: any) => {
+
+          ( Result: any ) => {
+
             console.log("api loaded");
+
             this.apiLoaded = true;
+
             return this.serviceDoc.initClient()
+
           },
-          (err: any) => {
+
+          ( err: any  ) => {
+
             this.apiFailed = true;
+
             console.log(err);
+
           }
-        ).then((result: any) => {
+
+        ).then( ( Result: any ) => {
+
           console.log("api ready");
+
           this.apiReady = true;
+
           this.serviceDoc.run();
-        }, (err: any) => {
+
+        }, ( err: any ) => {
+
           this.apiFailed = true;
+
           console.log(err);
+
         });
+
       },
-      (Err: any) => {
+
+      ( Err: any ) => {
+
           this.authFailed = true;
+
           console.log(Err);
+          
       }
+
     )
 
   }
